bccb38df2122e020a84f399aaadfb2a7f156c1c9
Sindre Moen <sindrenm@gmail.com>
2014-04-06 01:48:30 +0200

    Fix class names in factories

--- spec/factories/oversight/regions.rb
+++ spec/factories/oversight/regions.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
 
-  factory :oversight_region, class: 'Region' do
+  factory :oversight_region, class: 'Oversight::Region' do
     sequence(:name) { |n| "Region ##{n}" }
   end
 
--- spec/factories/oversight/stores.rb
+++ spec/factories/oversight/stores.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
 
-  factory :oversight_store, class: 'Store' do
+  factory :oversight_store, class: 'Oversight::Store' do
     association :region, factory: :oversight_region
     sequence(:name) { |n| "Store ##{n}" }
   end
