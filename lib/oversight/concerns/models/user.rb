810da0a820010de24f42b8e0713b55c1b6734473
Sindre Moen <sindrenm@gmail.com>
2014-04-06 01:47:53 +0200

    Create a name method for users

--- lib/oversight/concerns/models/user.rb
+++ lib/oversight/concerns/models/user.rb
@@ -5,6 +5,10 @@ module Oversight
     included do
       validates :first_name, :last_name, presence: true
       validates :phone, :email, uniqueness: true
+
+      def name
+        [first_name, middle_names, last_name].compact.join " "
+      end
     end
   end
 end
--- spec/models/oversight/user_spec.rb
+++ spec/models/oversight/user_spec.rb
@@ -2,5 +2,32 @@ require 'spec_helper'
 
 module Oversight
   describe User do
+    describe '#name' do
+      context 'when it has a first name, middle name(s) and last name' do
+        subject do
+          create :oversight_user,
+            first_name: 'Illidan',
+            middle_names: '"The Betrayer"',
+            last_name: 'Stormrage'
+        end
+
+        it 'combines all names, separated with a single space' do
+          expect(subject.name).to eq 'Illidan "The Betrayer" Stormrage'
+        end
+      end
+
+      context 'when it has a first name and last name, but no middle name(s)' do
+        subject do
+          create :oversight_user,
+            first_name: 'Khelgar',
+            middle_names: nil,
+            last_name: 'Ironfist'
+        end
+
+        it 'combines all names, separated with a single space' do
+          expect(subject.name).to eq 'Khelgar Ironfist'
+        end
+      end
+    end
   end
 end
