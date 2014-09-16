99bf769c7b84a902af5e7799f5f756d48a795965
Sindre Moen <sindrenm@gmail.com>
2014-09-16 17:55:22 +0200

    Refactor setting teams in controller

--- lib/oversight/concerns/controllers/teams_controller.rb
+++ lib/oversight/concerns/controllers/teams_controller.rb
@@ -2,9 +2,26 @@ module Oversight
   module Concerns::Controllers::TeamsController
     extend ActiveSupport::Concern
 
-    included { before_action :set_user }
+    included do
+      before_action :set_user
+
+      before_action :set_teams, only: :index
+      before_action :set_team, only: [ :show, :edit, :update ]
+    end
 
     def index
+    end
+
+    def show
+    end
+
+    private
+
+    def set_user
+      @user = Oversight::User.find params[:user_id] if params[:user_id]
+    end
+
+    def set_teams
       @teams = if @user
                  @user.teams
                else
@@ -12,18 +29,12 @@ module Oversight
                end
     end
 
-    def show
+    def set_team
       @team = if @user
                 @user.teams.find params[:id]
               else
                 Oversight::Team.find params[:id]
               end
-    end
-
-    private
-
-    def set_user
-      @user = Oversight::User.find params[:user_id] if params[:user_id]
     end
   end
 end
