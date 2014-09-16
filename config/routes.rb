f19f72649fd670b0558bcee0d470cfd40342a053
Sindre Moen <sindrenm@gmail.com>
2014-09-16 16:55:06 +0200

    Allow for nesting teams in users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Oversight::Engine.routes.draw do
-  resources :users, only: %i(show edit)
   resources :teams, only: %i(index show)
+
+  resources :users, only: %i(show edit) do
+    resources :teams, only: %i(index show)
+  end
 end
--- lib/oversight/concerns/controllers/teams_controller.rb
+++ lib/oversight/concerns/controllers/teams_controller.rb
@@ -2,12 +2,28 @@ module Oversight
   module Concerns::Controllers::TeamsController
     extend ActiveSupport::Concern
 
+    included { before_action :set_user }
+
     def index
-      @teams = Oversight::Team.all
+      @teams = if @user
+                 @user.teams
+               else
+                 Oversight::Team.all
+               end
     end
 
     def show
-      @team = Oversight::Team.find params[:id]
+      @team = if @user
+                @user.teams.find params[:id]
+              else
+                Oversight::Team.find params[:id]
+              end
+    end
+
+    private
+
+    def set_user
+      @user = Oversight::User.find params[:user_id] if params[:user_id]
     end
   end
 end
